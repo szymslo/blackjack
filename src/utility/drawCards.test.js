00dffb76c6e6a8b5b10a68f46c4f0c67fa08c0f0
Szymon Słonka <szymslo@laptop>
2021-03-03 23:26:25 +0100

    v 1.4.1 fixed bug in tests

--- src/utility/drawCards.test.js
+++ src/utility/drawCards.test.js
@@ -1,17 +1,15 @@
 import drawCards from "./drawCards"
-import shuffleCards from "./drawCards"
+import shuffleCards from "./shuffleCards"
 
 test('cards are drawn propely', async () => {
     const ID = await shuffleCards();
     const cards = await drawCards(ID, 4);
-    expect(cards).toEqual(expect.objectContaining([{
-        image: expect.any(String),
-        value: expect.any(String),
-        suit: expect.any(String),
-        code: expect.any(String),
-    }])
- )
+    expect(cards).toEqual(expect.arrayContaining([
+        expect.objectContaining({
+            image: expect.any(String),
+            value: expect.any(String),
+            suit: expect.any(String),
+            code: expect.any(String),})
+        ])
+    )
 })
-
-
-
--- src/utility/shuffleCards.js
+++ src/utility/shuffleCards.js
@@ -1,9 +1,7 @@
 const shuffleCards = async () => {
     try {
       const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6');
-      console.log(response)
       const json = await response.json();
-      console.log(json)
       return json.deck_id
     }
     catch(err) {
